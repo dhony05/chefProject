a53e92f1fe01ca6dfb2cb2e781e3b7e57a17bc35
Abraham Pierrot <pierrota@merrimack.edu>
2019-10-23 15:09:48 -0400

    add remaining code for account component

--- angular/angularclient/src/app/components/abe/account/account-routing.module.ts
+++ angular/angularclient/src/app/components/abe/account/account-routing.module.ts
@@ -1,12 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {  RouterModule,Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 
+
+const routes: Routes = [
+{path:'home', component:AccountComponent},
+{path:'user-login', component:LoginComponent},
+{path:'user-registration', component:UserRegistrationComponent},
+{path:'', redirectTo:'/home', pathMatch:'full'}
+/*,{path:'**', component:pageNotFoundComponent}*/
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
+  ], 
+  exports: [
+    RouterModule
   ]
 })
-export class AccountRoutingModule { }
+
+
+export class AccountRoutingModule {
+
+
+}
