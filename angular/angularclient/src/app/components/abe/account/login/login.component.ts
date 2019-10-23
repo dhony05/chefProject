4ac49bc6511bdbb1a6350768354b8ac0a304d40e
Abraham Pierrot <pierrota@merrimack.edu>
2019-10-23 14:44:18 -0400

    home page and user registration front-end

---
+++ angular/angularclient/src/app/components/abe/account/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email:string;
+  passwordd:string;
+
+  constructor() { }
+
+  validateData(){
+    //check if the information is right with the back end if so send user to store page 
+  }
+
+  ngOnInit() {
+  }
+
+}
